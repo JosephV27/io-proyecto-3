8907172a23047188330c791c879463a37ea59ee0
JosephV27 <jvalencianom08@estudiantec.cr>
2021-10-16 16:59:28 -0600

    Adding russell's method

--- transporte.py
+++ transporte.py
@@ -1,11 +1,8 @@
-from re import U
 import sys
-from mpmath.functions.functions import re
 import numpy as np
 import copy
 from sympy import Symbol, linsolve
 import pandas as pd
-from collections import Counter
 
 cost_matrix = []
 allocation_matrix = []
@@ -65,7 +62,7 @@ def north_west_corner() -> None:
             j += 1
 
 # take the diff beetwen the two min numbers of each row and column 
-def get_diff_beetwen_mins_in_rows(rows_ignored, cols_ignored):
+def get_diff_beetwen_mins_in_rows(rows_ignored, cols_ignored) -> list:
     global cost_matrix
     diff_in_rows = []
     for i in range(len(cost_matrix)):
@@ -87,7 +84,7 @@ def get_diff_beetwen_mins_in_rows(rows_ignored, cols_ignored):
             diff_in_rows.append(mins[1] - mins[0])
     return diff_in_rows
 
-def get_diff_beetwen_mins_in_cols(rows_ignored, cols_ignored):
+def get_diff_beetwen_mins_in_cols(rows_ignored, cols_ignored) -> list:
     global cost_matrix
     diff_in_cols = []
     for i in range(len(cost_matrix[0])):
@@ -109,7 +106,7 @@ def get_diff_beetwen_mins_in_cols(rows_ignored, cols_ignored):
             diff_in_cols.append(mins[1] - mins[0])
     return diff_in_cols
 
-def get_max(diff_in_rows, diff_in_cols):
+def get_max(diff_in_rows, diff_in_cols) -> tuple:
     max_rows = np.nanmax(np.array(diff_in_rows))
     max_cols = np.nanmax(np.array(diff_in_cols))
     if max_rows > max_cols :
@@ -119,7 +116,7 @@ def get_max(diff_in_rows, diff_in_cols):
     else:
         return (diff_in_rows, True)
 
-def get_min_index_of_row(row, cols_ignored):
+def get_min_index_of_row(row, cols_ignored) -> int:
     new_row = []
     for i in range(len(row)):
         if i in cols_ignored:
@@ -128,7 +125,7 @@ def get_min_index_of_row(row, cols_ignored):
             new_row.append(row[i])
     return new_row.index(np.nanmin(np.array(new_row)))
 
-def get_min_index_of_col(col, rows_ignored):
+def get_min_index_of_col(col, rows_ignored) -> int:
     new_col = []
     for i in range(len(col)):
         if i in rows_ignored:
@@ -137,7 +134,6 @@ def get_min_index_of_col(col, rows_ignored):
             new_col.append(col[i])
     return new_col.index(np.nanmin(np.array(new_col)))
  
-
 def vogel() -> None:
     global u
     supply_copy = copy.deepcopy(supply)
@@ -192,24 +188,66 @@ def vogel() -> None:
                 # insert rows finished
                 rows_ignored.append(min_col)
 
-def get_u_list() -> list:
-    global allocation_matrix
-    u_list = []
-    for i in range(len(allocation_matrix)):
-        u_list.append(Symbol("u" + str(i)))
-    return u_list
+def calculate_cost_indeces() -> list:
+    cost_indices = []
+    for i in range(len(cost_matrix)):
+        max_cost_row = max(cost_matrix[i])
+        for j in range(len(cost_matrix[0])):
+            max_cost_col = max(cost_matrix[:,j])
+            ic = max_cost_row + max_cost_col - cost_matrix[i][j]
+            cost_indices.append([ic, (i,j)])
+    return cost_indices
 
-def get_v_list() -> list:
+def get_cost_indices_by_priority() -> list:
+    cost_indices = calculate_cost_indeces()
+    cost_indices.sort(reverse=True)
+    return cost_indices
+
+def russell() -> None:
+    global u
+    cost_indices_by_priority = get_cost_indices_by_priority()
+    supply_copy = copy.deepcopy(supply)
+    demand_copy = copy.deepcopy(demand)
+    for i in range(len(cost_indices_by_priority)):
+        if supply_copy.count(0) == len(supply_copy) and \
+           demand_copy.count(0) == len(demand_copy):
+           break
+        supply_index = cost_indices_by_priority[i][1][0]
+        demand_index = cost_indices_by_priority[i][1][1]
+        if supply_copy[supply_index] == 0 or demand_copy[demand_index] == 0:
+            continue
+        else:
+            if supply_copy[supply_index] == 0:
+                minimun = demand_copy[demand_index]
+            elif demand_copy[demand_index] == 0:
+                minimun = supply_copy[supply_index]
+            else:
+                minimun = min(demand_copy[demand_index], supply_copy[supply_index])
+
+            supply_copy[supply_index] -= minimun
+            demand_copy[demand_index] -= minimun
+            allocation_matrix[cost_indices_by_priority[i][1]] = minimun
+            u += minimun * cost_matrix[cost_indices_by_priority[i][1]]
+
+
+def get_u_headers() -> None:
     global allocation_matrix
-    v_list = []
+    u_headers = []
+    for i in range(len(allocation_matrix)):
+        u_headers.append(Symbol("u" + str(i)))
+    return u_headers 
+
+def get_v_headers() -> list:
+    global allocation_matrix
+    v_headers = []
     for i in range(len(allocation_matrix[0])):
-        v_list.append(Symbol("v" + str(i)))
-    return v_list
+        v_headers.append(Symbol("v" + str(i)))
+    return v_headers
 
 def get_value_with_zero() -> str:
     # find the value with more allocations
-    u_list = get_u_list()
-    v_list = get_v_list()
+    u_headers = get_u_headers()
+    v_headers = get_v_headers()
     amount_alloc_in_us = []
     amount_alloc_in_vs = []
     for i in range(len(allocation_matrix)):
@@ -221,16 +259,16 @@ def get_value_with_zero() -> str:
     max_u = max(amount_alloc_in_us)
     max_v = max(amount_alloc_in_vs)
     if max_u > max_v:
-        u_list[amount_alloc_in_us.index(max_u)] = 0
-        return u_list, v_list
+        u_headers[amount_alloc_in_us.index(max_u)] = 0
+        return u_headers, v_headers
     elif max_u < max_v:
-        v_list[amount_alloc_in_vs.index(max_v)] = 0
-        return u_list, v_list
+        v_headers[amount_alloc_in_vs.index(max_v)] = 0
+        return u_headers, v_headers
     else:
-        u_list[amount_alloc_in_us.index(max_u)] = 0
-        return u_list, v_list
+        u_headers[amount_alloc_in_us.index(max_u)] = 0
+        return u_headers, v_headers
 
-def get_allocation_indices():
+def get_allocation_indices() -> list:
     global allocation_matrix
     allocation_indices = []
     for i in range(len(allocation_matrix)):
@@ -239,31 +277,31 @@ def get_allocation_indices():
                 allocation_indices.append((i,j))
     return allocation_indices
 
-def find_equations():
-    u_list, v_list = get_value_with_zero()
+def find_equations() -> list:
+    u_headers, v_headers = get_value_with_zero()
     allocation_indices = get_allocation_indices()
     equations = []
     for i, j in allocation_indices:
-        u = u_list[i]
-        v = v_list[j]
+        u = u_headers[i]
+        v = v_headers[j]
         c = cost_matrix[i][j]
         equation = u + v - c
         equations.append(equation)
-    sol = linsolve(equations, (u_list + v_list)).args[0]
-    amount_of_us = len(u_list)
-    sol_u = sol[:amount_of_us]
-    sol_v = sol[amount_of_us:]
-    return sol_u, sol_v
+    sol = linsolve(equations, (u_headers + v_headers)).args[0]
+    amount_of_us = len(u_headers)
+    u_sols = sol[:amount_of_us]
+    v_sols = sol[amount_of_us:]
+    return u_sols, v_sols
 
-def fill_indicators_matrix():
-    sol_u, sol_v = find_equations()
+def fill_indicators_matrix() -> None:
+    u_sols, v_sols = find_equations()
     for i in range(len(allocation_matrix)):
         for j in range(len(allocation_matrix[0])):
             if np.isnan(allocation_matrix[i][j]):
-                equation = sol_u[i] + sol_v[j] - cost_matrix[i][j]
+                equation = u_sols[i] + v_sols[j] - cost_matrix[i][j]
                 indicators_matrix[i][j] = equation
 
-def find_the_cycle():
+def find_the_cycle() -> None:
     global indicators_matrix, allocation_matrix
     allocation_matrix_copy = copy.deepcopy(allocation_matrix)
     indicators_matrix = np.array(indicators_matrix)
@@ -296,7 +334,7 @@ def find_the_cycle():
 # saco el menor de los impares 
 # ese valor es el que tengo que sumar y restar
 
-def change_variables():
+def change_variables() -> None:
     allocation_matrix_copy, max_indicator_index = find_the_cycle()
     allocation_order = []
     max_indicator_copy = copy.deepcopy(max_indicator_index)
@@ -345,19 +383,26 @@ def change_variables():
             allocation_matrix[index] = value + substaccion_value
     allocation_matrix[max_indicator_index] = substaccion_value
 
-def get_transport_solution():
+def get_transport_solution() -> None:
     allocation_indices = get_allocation_indices()
     global u 
     u = 0
     for i,j in allocation_indices:
         u += cost_matrix[i,j] * allocation_matrix[i,j]
 
-def is_optimal():
+def is_optimal() -> bool:
     return np.nanmax(indicators_matrix) < 0
 
-def write_transport_solution(iteration):
+def write_transport_solution(iteration: int) -> None:
+    u_headers, v_headers = get_u_headers(), get_v_headers()
+    u_headers = ["", *u_headers]
+    v_headers = ["", *v_headers]
+    u_sols, v_sols = find_equations()
+    u_sols = [np.NaN, *u_sols]
+    indicators_matrix_v = np.insert(indicators_matrix, 0, v_sols, axis=0)
+    indicators_matrix_u_v = np.insert(indicators_matrix_v, 0, u_sols, axis=1)
     allocation_matrix_df = pd.DataFrame(data=allocation_matrix)
-    indicators_matrix_df = pd.DataFrame(data=indicators_matrix)
+    indicators_matrix_df = pd.DataFrame(data=indicators_matrix_u_v, columns=v_headers, index=u_headers)
     new_file = '{0}_solution.txt'.format(filename)
     with open(new_file, "a") as f:
         txt_sol = f'Transport Algorithm\n\n' \
@@ -370,7 +415,7 @@ def write_transport_solution(iteration):
         f.write(txt_sol)
     f.close
 
-def transport():
+def transport() -> None:
     global indicators_matrix
     indicators_matrix = np.full((len(cost_matrix), len(cost_matrix[0])), np.NaN)
     i = 0
@@ -396,7 +441,7 @@ def get_headers() -> list:
     cols_header.append("Supply")
     return rows_header, cols_header
 
-def write_initial_solution(method):
+def write_initial_solution(method: str) -> None:
     rows_header, cols_header = get_headers()
     supply_copy = copy.deepcopy(supply)
     supply_copy.append(np.NaN)
@@ -426,12 +471,14 @@ if __name__ == "__main__":
         north_west_corner()
         write_initial_solution("NORTHWEST CORNER")
         transport()
-    if method == "2":
+    elif method == "2":
         vogel()
         write_initial_solution("VOGEL")
         transport()
-
+    elif method == "3":
+        russell()
+        write_initial_solution("RUSSELL")
+        transport()
+    
     #TODO debug p1.txt 
-    #TODO print v's and u's
-    #TODO implement Russell
 
